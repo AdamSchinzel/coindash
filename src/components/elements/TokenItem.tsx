36d97e1b2c0db6b98997463effa7fc2e4291972f
AdamSchinzel <schinzel.adam@gmail.com>
2023-06-02 03:00:08 +0200

    fix: margin

--- src/components/elements/TokenItem.tsx
+++ src/components/elements/TokenItem.tsx
@@ -8,7 +8,7 @@ const TokenItem = ({ name, symbol, contractAddress, balance, price }: Token) => 
   return (
     <Flex justifyContent="flex-start" width="100%">
       <Flex flex={1}>
-        <Text display={["none", "inline", "inline", "inline"]}>{`${name}`}</Text>
+        <Text display={["none", "inline", "inline", "inline"]} mr={2}>{`${name}`}</Text>
         {contractAddress && (
           <Link href={`https://etherscan.io/address/${contractAddress}`} target="_blank">
             <ExternalLinkIcon cursor="pointer" />

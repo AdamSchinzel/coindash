27e134c2da7f73b526fd3916a6f5d512b7d9facf
AdamSchinzel <schinzel.adam@gmail.com>
2023-07-01 18:28:32 +0200

    chore: add store description

--- src/stores/useWalletsStore.ts
+++ src/stores/useWalletsStore.ts
@@ -7,6 +7,12 @@ interface WalletsState {
   deleteWallet: (wallet: string) => void;
 }
 
+/**
+ * State management store for connected wallets
+ * @method addWallet - A function that enables a new wallet to be added to the wallet array on the state
+ * @method deleteWallet - A function that enables a wallet to be deleted from the wallet array on the state
+ * @returns {Object} - An object containing the current state, along with `addWallet` and `deleteWallet` function
+ */
 const useWalletsStore = create<WalletsState>()(
   persist(
     (set) => ({
